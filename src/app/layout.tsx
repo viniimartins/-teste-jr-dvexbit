a114a0e6d52129d16b7fabfb4ea1932624d1b5dc
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2025-03-08 14:14:28 -0300

    feat: tables prisma user and tasks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
-import './globals.css'
+import '@/styles/globals.css'
 
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+
+import { Providers } from '@/providers'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   )
